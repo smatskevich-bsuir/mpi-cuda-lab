c60cee1a1aadf737973614f617fff5249670e17a
Stanislau Matskevich <st.matskevich.bsuir@gmail.com>
2021-05-20 00:26:57 +0300

    Added headers guards

--- include/file_queue.hpp
+++ include/file_queue.hpp
@@ -1,4 +1,9 @@
+#ifndef FILE_QUEUE_HPP
+#define FILE_QUEUE_HPP
+
 #include <unordered_set>
 #include <vector>
 
 std::vector<std::string> get_task_files(const size_t rank, const size_t cluster_size, const std::string& directory, const std::unordered_set<std::string>& allowed_extensions = { ".png", ".bmp", ".jpg", ".jpeg" });
+
+#endif
