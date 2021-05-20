9295f5910a8607d18489224e4e5f1892944b1a23
Stanislau Matskevich <st.matskevich.bsuir@gmail.com>
2021-05-21 01:19:47 +0300

    Added shared memory

--- src/image_processing.cu
+++ src/image_processing.cu
@@ -31,23 +31,48 @@ __device__ inline int prewitt_gradient(u_char p11, u_char p12, u_char p13, u_cha
 
 __global__ void prewitt_cylce_1ch(const u_char* data, u_char* res, const size_t width, const size_t height, const size_t pitch_in, const size_t pitch_out) 
 {
-	size_t offset_x = (CH1_WINDOW_SIZE.x - 2) * (blockIdx.x * blockDim.x + threadIdx.x);
-	size_t offset_y = (CH1_WINDOW_SIZE.y - 2) * (blockIdx.y * blockDim.y + threadIdx.y);
+	const dim3 offset_block(blockIdx.x * blockDim.x * (CH1_WINDOW_SIZE.x - 2), blockIdx.y * blockDim.y * (CH1_WINDOW_SIZE.y - 2));
+	const dim3 offset_thread(threadIdx.x * (CH1_WINDOW_SIZE.x - 2), threadIdx.y * (CH1_WINDOW_SIZE.y - 2));
+
+	//memory for block with align to 4 bytes
+	const size_t shared_pixels_width = ((CH1_WINDOW_SIZE.x + (CH1_BLOCK_SIZE.x - 1) * (CH1_WINDOW_SIZE.x - 2) + sizeof(u_int32_t) - 1) / sizeof(u_int32_t)) * sizeof(u_int32_t);
+	const size_t shared_pixels_height = CH1_WINDOW_SIZE.y + (CH1_BLOCK_SIZE.y - 1) * (CH1_WINDOW_SIZE.y - 2);
+	__shared__ u_char shared_pixels[shared_pixels_height][shared_pixels_width];
+
+	const size_t real_pixels_width = min(shared_pixels_width, width - offset_block.x);
+	const size_t real_pixels_height = min(shared_pixels_height, height - offset_block.y);
+
+	const size_t copy_threads_x = (real_pixels_width + sizeof(u_int32_t) - 1) / sizeof(u_int32_t);
+	const size_t copy_threads_y = real_pixels_height;
+
+	//use this and add additional reads for threadIdx.y = 0 and 1
+	/*if(threadIdx.x < copy_threads_x && threadIdx.y < copy_threads_y)
+		memcpy(shared_pixels[threadIdx.y] + threadIdx.x * sizeof(u_int32_t), data + (offset_block.y + threadIdx.y) * pitch_in + offset_block.x + threadIdx.x * sizeof(u_int32_t), sizeof(u_int32_t));*/
+
+	//or this but with % and /
+	const size_t idx = threadIdx.x + threadIdx.y * blockDim.x;
+	const size_t read_y = idx / copy_threads_x;
+	const size_t read_x = idx % copy_threads_x;
+	if(read_x < copy_threads_x && read_y < copy_threads_y)
+		memcpy(shared_pixels[read_y] + read_x * sizeof(u_int32_t), data + (offset_block.y + read_y) * pitch_in + offset_block.x + read_x * sizeof(u_int32_t), sizeof(u_int32_t));
+
+	__syncthreads();
+
 	
 	//can calculate at least 3x3
-	if(offset_x + 2 < width && offset_y + 2 < height)
+	if(offset_block.x + offset_thread.x + 2 < width && offset_block.y + offset_thread.y + 2 < height)
 	{
 		//hardcode for WINDOW_SIZE dim3(4, 3, 1);
 		u_int32_t words[3];
-		memcpy(words, data + offset_x + offset_y * pitch_in, sizeof(u_int32_t));
-		memcpy(words + 1, data + offset_x + (offset_y + 1) * pitch_in, sizeof(u_int32_t));
-		memcpy(words + 2, data + offset_x + (offset_y + 2) * pitch_in, sizeof(u_int32_t));
+		memcpy(words, shared_pixels[offset_thread.y] + offset_thread.x, sizeof(u_int32_t));
+		memcpy(words + 1, shared_pixels[offset_thread.y + 1] + offset_thread.x, sizeof(u_int32_t));
+		memcpy(words + 2, shared_pixels[offset_thread.y + 2] + offset_thread.x, sizeof(u_int32_t));
 
 		u_char out[2];
 		out[0] = prewitt_gradient(words[0] >> 24 & 0xFF, words[0] >> 16 & 0xFF, words[0] >> 8 & 0xFF, words[1] >> 24 & 0xFF, words[1] >> 16 & 0xFF, words[1] >> 8 & 0xFF, words[2] >> 24 & 0xFF, words[2] >> 16 & 0xFF, words[2] >> 8 & 0xFF);
 		out[1] = prewitt_gradient(words[0] >> 16 & 0xFF, words[0] >> 8 & 0xFF, words[0] & 0xFF, words[1] >> 16 & 0xFF, words[1] >> 8 & 0xFF, words[1] & 0xFF, words[2] >> 16 & 0xFF, words[2] >> 8 & 0xFF, words[2] & 0xFF);
-	
-		memcpy(res + offset_x + 1 + (offset_y + 1) * pitch_out, out, 2 * sizeof(u_char));
+
+		memcpy(res + offset_block.x + offset_thread.x + 1 + (offset_block.y + offset_thread.y + 1) * pitch_out, out, 2 * sizeof(u_char));
 	}
 }
 
