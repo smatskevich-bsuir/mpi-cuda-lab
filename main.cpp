5f936b15d7d009e136c12e9692de270d3d8ada31
Stanislau Matskevich <st.matskevich.bsuir@gmail.com>
2021-05-20 17:49:11 +0300

    Added time measurement

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <mpi.h>
 #include <iostream> 
 #include <sstream>
+#include <chrono>
 
 #include "include/file_queue.hpp"
 #include "include/image_processing.cuh"
+
+typedef std::chrono::time_point<std::chrono::steady_clock> _time_t;
 
 const char* TASK_DIRECTORY = "/cluster/task";
 
@@ -11,6 +14,7 @@ int main (int argc, char* argv[])
 {
     int rank;
     int cluster_size;
+    _time_t program_start = std::chrono::steady_clock::now();
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &cluster_size); 
@@ -27,8 +31,21 @@ int main (int argc, char* argv[])
     info << std::endl;
     std::cout << info.str();
 
+    info.str(std::string());
+    info << proc_name << " report: ";
     for(auto file: files)
+    {
+        _time_t file_start = std::chrono::steady_clock::now();   
         prewittGPU(file);
+        _time_t file_end = std::chrono::steady_clock::now(); \
+        float time = std::chrono::duration<float>(file_end - file_start).count() * 1000;
+        info << file << "(" << time << "ms) ";
+    }
+
+    _time_t program_end = std::chrono::steady_clock::now();
+	float time = std::chrono::duration<float>(program_end - program_start).count() * 1000;
+    info << "full time: " << time << "ms" << std::endl;
+    std::cout << info.str();
 
     MPI_Finalize();
     return 0;
